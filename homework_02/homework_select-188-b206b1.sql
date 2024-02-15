8aa678a172d14cc4e6df49e7c150d2ff24c73acb
Alexander <134145027+totkin@users.noreply.github.com>
2024-02-15 17:54:53 +0300

    Init hw02

--- homework_02/homework_select-188-b206b1.sql
+++ homework_02/homework_select-188-b206b1.sql
@@ -36,6 +36,7 @@ where StockItemName like '%urgent%' or StockItemName like  'Animal%'
 Таблицы: Purchasing.Suppliers, Purchasing.PurchaseOrders.
 По каким колонкам делать JOIN подумайте самостоятельно.
 */
+
 select PS.SupplierID,PS.SupplierName
 from  Purchasing.Suppliers as PS		
 	  left outer join Purchasing.PurchaseOrders as PPO on PS.SupplierID=PPO.SupplierID
@@ -62,17 +63,28 @@ group by PS.SupplierID,PS.SupplierName
 Таблицы: Sales.Orders, Sales.OrderLines, Sales.Customers.
 */
 
-select
-	OrderID,
-	format(OrderDate,'dd.mm.YYYY') as OrderDate,
-	format(OrderDate,'mm','ru-RU') as [название месяца, в котором был сделан заказ],
-	DATEPART(QUARTER, OrderDate) as [номер квартала, в котором был сделан заказ],
-	 as [треть года, к которой относится дата заказа],
-	Customer
 
+DECLARE @OFF BIGINT=1000, @FETCH BIGINT=100
 
-from Orders
-where UnitPrice>100 or Quantity>20
+SELECT
+	OO.OrderID,
+	format(OO.OrderDate,'dd.MM.yyyy')      AS OrderDate,
+	format(OO.OrderDate,'MMMM','ru-RU')    AS [название месяца, в котором был сделан заказ],
+	DATEPART(QUARTER, OO.OrderDate)        AS [номер квартала, в котором был сделан заказ],
+	CASE WHEN MONTH(OO.OrderDate)<5 THEN 1
+		 WHEN MONTH(OO.OrderDate)<9 THEN 2
+		 ELSE 3 END                        AS [треть года, к которой относится дата заказа],
+	CC.CustomerName    
+FROM
+	Sales.OrderLines AS OL
+	INNER JOIN Sales.Orders AS OO ON OL.OrderID = OO.OrderID
+	INNER JOIN Sales.Customers AS CC ON OO.CustomerID = CC.CustomerID
+WHERE OL.UnitPrice>100 or OL.Quantity>20
+GROUP BY OO.OrderID,OO.OrderDate,CC.CustomerName
+ORDER BY 4,5,OO.OrderDate
+	OFFSET @OFF ROWS
+    FETCH NEXT @FETCH ROWS ONLY
+
 
 /*
 4. Заказы поставщикам (Purchasing.Suppliers),
@@ -88,7 +100,22 @@ where UnitPrice>100 or Quantity>20
 Таблицы: Purchasing.Suppliers, Purchasing.PurchaseOrders, Application.DeliveryMethods, Application.People.
 */
 
-select DeliveryMethodName,ExpectedDeliveryDate
+SELECT DISTINCT
+	PO.PurchaseOrderID,  --тут вариант оставил, если ID не нужно, можно закомментить строку и более поятно сработает DISTINCT
+	DM.DeliveryMethodName as [способ доставки],
+	PO.ExpectedDeliveryDate as [дата доставки],
+	SS.SupplierName as [имя поставщика],
+	PP.FullName as [имя контактного лица принимавшего заказ]  
+FROM
+	Purchasing.Suppliers AS SS
+	INNER JOIN Purchasing.PurchaseOrders AS PO ON SS.SupplierID = PO.SupplierID
+	INNER JOIN Application.People AS PP ON PO.ContactPersonID = PP.PersonID
+	INNER JOIN Application.DeliveryMethods AS DM ON PO.DeliveryMethodID = DM.DeliveryMethodID
+WHERE
+	YEAR(PO.ExpectedDeliveryDate)*100 + MONTH(PO.ExpectedDeliveryDate)=201301
+	AND DM.DeliveryMethodName in (N'Air Freight' , N'Refrigerated Air Freight')
+	AND PO.IsOrderFinalized=1
+
 
 /*
 5. Десять последних продаж (по дате продажи) с именем клиента и именем сотрудника,
@@ -96,7 +123,7 @@ select DeliveryMethodName,ExpectedDeliveryDate
 Сделать без подзапросов.
 */
 
-напишите здесь свое решение
+
 
 /*
 6. Все ид и имена клиентов и их контактные телефоны,
