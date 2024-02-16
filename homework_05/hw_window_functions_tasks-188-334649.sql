102a6cea9cf351d736c807ae03bed4828ef21ee5
Alexander <134145027+totkin@users.noreply.github.com>
2024-02-16 23:59:11 +0300

    hw05 work

--- homework_05/hw_window_functions_tasks-188-334649.sql
+++ homework_05/hw_window_functions_tasks-188-334649.sql
@@ -38,21 +38,113 @@ USE WideWorldImporters
 Нарастающий итог должен быть без оконной функции.
 */
 
-напишите здесь свое решение
+-- вариант с табличной переменной
+---------------------------------------------------------------------------------------------------------
+SET STATISTICS TIME ON;
+
+DECLARE @TABLE_OTUS TABLE
+      (YM INT null,
+       [сумма продажи] DECIMAL(18,3) null
+       )
+
+INSERT @TABLE_OTUS (YM,[сумма продажи])
+SELECT
+	YEAR(SUB_SI.InvoiceDate)*100+MONTH(SUB_SI.InvoiceDate) AS YM,
+	SUM(SUB_IL.UnitPrice * SUB_IL.Quantity)                AS [сумма продажи]
+FROM Sales.Invoices                 AS SUB_SI
+	 INNER JOIN Sales.InvoiceLines  AS SUB_IL ON SUB_SI.InvoiceID = SUB_IL.InvoiceID
+GROUP BY YEAR(SUB_SI.InvoiceDate)*100+MONTH(SUB_SI.InvoiceDate)
+ORDER BY 1
+
+SELECT
+	SI.InvoiceID as [id продажи],
+	CC.CustomerName as [название клиента],
+	SI.InvoiceDate as [дата продажи],
+	SUM(IL.UnitPrice * IL.Quantity) AS [сумма продажи],
+	(SELECT SUM([сумма продажи]) FROM @TABLE_OTUS WHERE YM<=YEAR(SI.InvoiceDate)*100+MONTH(SI.InvoiceDate)) AS [сумма нарастающим итогом]
+FROM
+	Sales.Invoices                AS SI
+	INNER JOIN Sales.InvoiceLines AS IL ON SI.InvoiceID = IL.InvoiceID
+	INNER JOIN Sales.Customers    AS CC ON SI.CustomerID = CC.CustomerID	
+GROUP BY SI.InvoiceID,CC.CustomerName,SI.InvoiceDate
+
+SET STATISTICS TIME OFF; 
+
+/* ------------------------------------------------------- вывод
+ SQL Server Execution Times:
+   CPU time = 56 ms,  elapsed time = 60 ms.
+
+(41 rows affected)
+
+(70510 rows affected)
+
+ SQL Server Execution Times:
+   CPU time = 951 ms,  elapsed time = 981 ms.
+
+Completion time: 2024-02-16T22:24:34.8215722+03:00
+---------------------------------------------------------- вывод */
+
+
 
 /*
 2. Сделайте расчет суммы нарастающим итогом в предыдущем запросе с помощью оконной функции.
    Сравните производительность запросов 1 и 2 с помощью set statistics time, io on
 */
 
-напишите здесь свое решение
+SET STATISTICS TIME ON;
+
+SELECT
+	SI.InvoiceID as [id продажи],
+	CC.CustomerName as [название клиента],
+	SI.InvoiceDate as [дата продажи],
+	SUM(IL.UnitPrice * IL.Quantity) AS [сумма продажи],
+	SUM(SUM(IL.UnitPrice * IL.Quantity))
+		over(partition by YEAR(SI.InvoiceDate)*100+MONTH(SI.InvoiceDate)
+			 ) as  [сумма нарастающим итогом]
+FROM
+	Sales.Invoices                AS SI
+	INNER JOIN Sales.InvoiceLines AS IL ON SI.InvoiceID = IL.InvoiceID
+	INNER JOIN Sales.Customers    AS CC ON SI.CustomerID = CC.CustomerID
+GROUP BY SI.InvoiceID,CC.CustomerName,SI.InvoiceDate
+
+SET STATISTICS TIME OFF;
+
+
+/* ------------------------------------------------------- вывод
+ SQL Server Execution Times:
+   CPU time = 57 ms,  elapsed time = 59 ms.
+
+(41 rows affected)
+
+(70510 rows affected)
+
+ SQL Server Execution Times:
+   CPU time = 965 ms,  elapsed time = 997 ms.
+
+Completion time: 2024-02-16T22:17:08.4708181+03:00
+---------------------------------------------------------- вывод */
+
+
 
 /*
 3. Вывести список 2х самых популярных продуктов (по количеству проданных) 
 в каждом месяце за 2016 год (по 2 самых популярных продукта в каждом месяце).
 */
 
-напишите здесь свое решение
+select [month],[rate],StockItemID, StockItemName
+from (
+	SELECT
+		SI.StockItemID, SI.StockItemName, month([IS].InvoiceDate) as [month], sum(IL.Quantity*IL.UnitPrice) as [value],
+		DENSE_RANK()over (partition by month([IS].InvoiceDate) order by sum(IL.Quantity*IL.UnitPrice) desc) as rate
+	FROM
+		Sales.Invoices AS [IS]
+		INNER JOIN Sales.InvoiceLines AS IL ON [IS].InvoiceID = IL.InvoiceID
+		INNER JOIN Warehouse.StockItems AS SI ON IL.StockItemID = SI.StockItemID
+	WHERE year([IS].InvoiceDate)=2016
+	GROUP BY SI.StockItemID, SI.StockItemName,month([IS].InvoiceDate)
+) as TT
+where rate<3
+order by [month],[rate]
 
 /*
 4. Функции одним запросом
@@ -68,20 +160,73 @@ USE WideWorldImporters
 Для этой задачи НЕ нужно писать аналог без аналитических функций.
 */
 
-напишите здесь свое решение
+
+with ITEMS([StockItemID],[StockItemName],First_Letter, [TypicalWeightPerUnit])
+as
+(
+	select
+		[StockItemID],[StockItemName],
+		left(replace([StockItemName],'"',''),1) as First_Letter,
+		[TypicalWeightPerUnit]
+	from [Warehouse].[StockItems]
+)
+
+select
+	[StockItemID],[StockItemName], 
+	DENSE_RANK()over (partition by First_Letter order by [StockItemName])      as [пронумеруйте записи по названию товара, так чтобы при изменении буквы алфавита нумерация начиналась заново],
+	count(*) over()                                                            as [общее количество товаров],
+	count(*) over(partition by First_Letter)                                   as [общее количество товаров в зависимости от первой буквы названия товара],
+	lead([StockItemID]) over (order by [StockItemName])                        as [следующий id товара исходя из того, что порядок отображения товаров по имени],
+	lag([StockItemID]) over (order by [StockItemName])                         as [предыдущий ид товара с тем же порядком отображения (по имени)],
+	isnull(lag([StockItemName],2) over (order by [StockItemName]),N'No items') as [названия товара 2 строки назад, в случае если предыдущей строки нет нужно вывести "No items"],
+	ntile(30) over(order by [TypicalWeightPerUnit])                            as [30 групп товаров по полю вес товара на 1 шт]
+
+from ITEMS
+order by [StockItemID],[StockItemName]
+
+
 
 /*
 5. По каждому сотруднику выведите последнего клиента, которому сотрудник что-то продал.
    В результатах должны быть ид и фамилия сотрудника, ид и название клиента, дата продажи, сумму сделки.
 */
 
-напишите здесь свое решение
+select PersonID,фамилия,CustomerID, CustomerName,InvoiceDate, [сумма сделки]
+from 
+(
+	SELECT
+		PP.PersonID, REPLACE(PP.FullName, PP.PreferredName + ' ', '') AS фамилия,
+		CC.CustomerID, CC.CustomerName, 
+		[IS].InvoiceDate,
+		sum(IL.Quantity*IL.UnitPrice) as [сумма сделки],
+		rank() over (partition by PP.PersonID order by [IS].InvoiceDate desc,[IS].InvoiceID desc ) as rate
+	FROM 
+		Sales.Invoices                 AS [IS]
+		INNER JOIN Sales.InvoiceLines  AS IL ON [IS].InvoiceID = IL.InvoiceID
+		INNER JOIN Sales.Customers     AS CC ON [IS].CustomerID = CC.CustomerID
+		INNER JOIN  Application.People AS PP ON [IS].SalespersonPersonID = PP.PersonID
+	GROUP BY PP.PersonID, REPLACE(PP.FullName, PP.PreferredName + ' ', ''),
+		CC.CustomerID, CC.CustomerName, 
+		[IS].InvoiceDate,[IS].InvoiceID
+) as TT where rate=1
 
 /*
 6. Выберите по каждому клиенту два самых дорогих товара, которые он покупал.
 В результатах должно быть ид клиета, его название, ид товара, цена, дата покупки.
 */
 
-напишите здесь свое решение
 
-Опционально можете для каждого запроса без оконных функций сделать вариант запросов с оконными функциями и сравнить их производительность. 
+select CustomerID, CustomerName, [StockItemID], UnitPrice, InvoiceDate
+from 
+(
+	SELECT
+		CC.CustomerID, CC.CustomerName, IL.[StockItemID], IL.UnitPrice, max([IS].InvoiceDate) as InvoiceDate,
+		RANK() over (partition by CC.CustomerID order by IL.UnitPrice desc, sum(IL.Quantity*IL.UnitPrice) desc) as rate
+	FROM 
+		Sales.Invoices                AS [IS]
+		INNER JOIN Sales.InvoiceLines AS IL ON [IS].InvoiceID = IL.InvoiceID
+		INNER JOIN Sales.Customers    AS CC ON [IS].CustomerID = CC.CustomerID
+	group by CC.CustomerID, CC.CustomerName, IL.[StockItemID], IL.UnitPrice
+) as TT where rate<3
+
+--Опционально можете для каждого запроса без оконных функций сделать вариант запросов с оконными функциями и сравнить их производительность. 
